af144d8b9c6259e998ad6138c3e001671fbc87a1
ekaapsd <epdewi26@gmail.com>
2019-07-09 11:52:33 +0700

    Add Model ListKemampuan&TambahanPendapatan

---
+++ BE/src/main/java/com/example/model/AdditionalIncomeModel.java
@@ -0,0 +1,26 @@
+package com.example.model;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name="Tambahan_pendapatan")
+public class AdditionalIncomeModel{
+	@Id
+	@Column(name="id_tambahan_pendapatan")
+	private long idAdditional;
+	@Column(name="nik")
+	private String nik;
+	@Column(name="tanggal_tambahan_pendapatan")
+	private String dateOfAdditionalIncome;
+	@Column(name="lama_lembur")
+	private long lengthOvertime;
+	@Column(name="variable_bonus")
+	private long bonusVariable;
+}
